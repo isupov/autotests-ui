62550549b7b648b1cd3b7ce88e7604443d543e99
isupov <isupovilya@gmail.com>
2025-06-03 20:45:44 +0700

    Фикстуры Pytest. Практика работы с pytest фикстурами

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,13 @@
 import pytest
 from playwright.sync_api import Playwright, Page
 
+@pytest.fixture
+def chromium_page(playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    yield browser.new_page()
+    browser.close()
+
+
 @pytest.fixture(scope='session')
 def initialize_browser_state(playwright: Playwright): # Фикстура для регистрации и сохранения пользователя
     # Открываем браузер и создаем новую страницу
