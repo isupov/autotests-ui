04cea7aa8d7c777ac44e3b55050df607c736e754
isupov <isupovilya@gmail.com>
2025-06-04 18:50:20 +0700

    Знакомство с PageObject. Практика работы с Page Object

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -2,6 +2,7 @@ import pytest
 from playwright.sync_api import Page
 
 from pages.login_page import LoginPage  # Импортируем LoginPage
+from pages.registration_page import RegistrationPage  # Импортируем RegistrationPage
 
 @pytest.mark.regression
 @pytest.mark.authorization
@@ -15,3 +16,13 @@ def test_wrong_email_or_password_authorization(login_page: LoginPage, email: str
     login_page.fill_login_form(email=email, password=password)
     login_page.click_login_button()
     login_page.check_visible_wrong_email_or_password_alert()
+
+@pytest.mark.regression
+@pytest.mark.authorization
+@pytest.mark.parametrize("email, username, password", [
+    ("user.name@gmail.com", "username", "password")
+])
+def test_successful_registration(registration_page: RegistrationPage, email: str, username: str, password: str):
+    registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    registration_page.fill_registration_form(email=email, username=username, password=password)
+    registration_page.click_registration_button()
