b2e0b463252f1d35fba93c4e5616775bbd60bf86
isupov <isupovilya@gmail.com>
2025-06-07 16:47:58 +0700

    Практикуемся в реализации PageComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -12,10 +12,7 @@ def test_empty_courses_list(courses_list_page: CoursesListPage):
     courses_list_page.navbar.check_visible("username")
     # Добавили проверку Sidebar компонента на странице Dashboard
     courses_list_page.sidebar.check_visible()
-    # Проверяем  наличие и текст заголовка "Courses"
-    courses_list_page.check_visible_courses_title()
-    # Проверяем  наличие кнопки создать курс
-    courses_list_page.check_visible_create_course_button()
+    courses_list_page.toolbar_view.check_visible()
     # Проверяем  наличие и текст заголовка  "There is no results"
     courses_list_page.check_visible_empty_view()
 
@@ -25,8 +22,7 @@ def test_create_course(courses_list_page: CoursesListPage, create_course_page: C
     create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
     create_course_page.check_visible_create_course_title()
     create_course_page.check_disabled_create_course_button()
-    create_course_page.check_visible_image_preview_empty_view()
-    create_course_page.check_visible_image_upload_view()
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
     create_course_page.check_visible_create_course_form(
         title="",
         estimated_time="",
@@ -37,8 +33,8 @@ def test_create_course(courses_list_page: CoursesListPage, create_course_page: C
     create_course_page.check_visible_exercises_title()
     create_course_page.check_visible_create_exercise_button()
     create_course_page.check_visible_exercises_empty_view()
-    create_course_page.upload_preview_image("./testdata/files/image.png")
-    create_course_page.check_visible_image_upload_view()
+    create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
     create_course_page.fill_create_course_form(
         title="Playwright",
         estimated_time="2 weeks",
@@ -47,12 +43,7 @@ def test_create_course(courses_list_page: CoursesListPage, create_course_page: C
         min_score="10"
     )
     create_course_page.click_create_course_button()
-    courses_list_page.check_visible_courses_title()
-    courses_list_page.check_visible_create_course_button()
-    courses_list_page.check_visible_course_card(
-        index=0,
-        title="Playwright",
-        max_score="100",
-        min_score="10",
-        estimated_time="2 weeks"
+    courses_list_page.toolbar_view.check_visible()
+    courses_list_page.course_view.check_visible(
+        index=0, title="Playwright", max_score="100", min_score="10", estimated_time="2 weeks"
     )
