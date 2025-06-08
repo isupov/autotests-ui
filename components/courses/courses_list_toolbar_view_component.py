eab32716b8cdf7f36d235bc7c8182b66e11eed89
isupov <isupovilya@gmail.com>
2025-06-08 07:30:06 +0700

    Практикуемся в реализации PageComponent. Практикуемся в написании компонентов с использованием паттерна PageComponent

--- components/courses/courses_list_toolbar_view_component.py
+++ components/courses/courses_list_toolbar_view_component.py
@@ -15,7 +15,6 @@ class CoursesListToolbarViewComponent(BaseComponent):
     def check_visible(self):
         expect(self.title).to_be_visible()
         expect(self.title).to_have_text('Courses')
-
         expect(self.create_course_button).to_be_visible()
 
     def click_create_course_button(self):
