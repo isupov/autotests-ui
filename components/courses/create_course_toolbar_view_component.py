12b3af70a3cf6088f9865821f3e7c4bb4b0bce80
isupov <isupovilya@gmail.com>
2025-06-10 19:19:13 +0700

    Практические применение PageFactory в автотестах. Практикуемся в применении Page Factory

--- components/courses/create_course_toolbar_view_component.py
+++ components/courses/create_course_toolbar_view_component.py
@@ -1,20 +1,21 @@
 from playwright.sync_api import Page, expect
 
 from components.base_component import BaseComponent
-
+from elements.button import Button
+from elements.text import Text
 
 class CreateCourseToolbarViewComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.create_course_title = page.get_by_test_id('create-course-toolbar-title-text')
-        self.create_course_button = page.get_by_test_id('create-course-toolbar-create-course-button')
+        self.create_course_title = Text(page,'create-course-toolbar-title-text', 'Title')
+        self.create_course_button = Button(page, 'create-course-toolbar-create-course-button', 'Create Button')
 
     def check_visible(self, is_create_course_disabled=True):
         if is_create_course_disabled:
-            expect(self.create_course_button).to_be_disabled()
+            self.create_course_button.check_disabled()
         if not is_create_course_disabled:
-            expect(self.create_course_button).to_be_enabled()
+            self.create_course_button.check_enabled()
 
     def click_create_course_button(self):
         self.create_course_button.click()
