131e8d7e2b690af3b33edde39b2843e5e91b3f7f
isupov <isupovilya@gmail.com>
2025-06-11 22:05:50 +0700

    Наращивание тестовой базы и рефакторинг

--- elements/button.py
+++ elements/button.py
@@ -7,7 +7,7 @@ class Button(BaseElement):
     def check_enabled(self, nth: int = 0, **kwargs):
         # Добавили аргумент nth и передеаем его в get_locator
         locator = self.get_locator(nth, **kwargs)
-        expect(locator).to_be_disabled()
+        expect(locator).to_be_enabled()
 
     def check_disabled(self, nth: int = 0, **kwargs):
         # Добавили аргумент nth и передеаем его в get_locator
