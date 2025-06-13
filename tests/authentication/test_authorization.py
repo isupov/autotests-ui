9c8f0c78984f69d045f4db157cde1260637f4988
isupov <isupovilya@gmail.com>
2025-06-13 18:25:58 +0700

    Интеграция Allure в автотесты

--- tests/authentication/test_authorization.py
+++ tests/authentication/test_authorization.py
@@ -1,12 +1,25 @@
 import pytest
+import allure
+from allure_commons.types import Severity # Импортируем enum Severity из Allure
 
 from pages.authentication.login_page import LoginPage
 from pages.authentication.registration_page import RegistrationPage
 from pages.dashboard.dashboard_page import DashboardPage
+from tools.allure.epics import AllureEpic # Импортируем enum AllureEpic
+from tools.allure.features import AllureFeature # Импортируем enum AllureFeature
+from tools.allure.stories import AllureStory # Импортируем enum AllureStory
+from tools.allure.tags import AllureTag
 
 
 @pytest.mark.regression
 @pytest.mark.authorization
+@allure.tag(AllureTag.REGRESSION, AllureTag.AUTHORIZATION)
+@allure.epic(AllureEpic.LMS) # Добавили epic
+@allure.feature(AllureFeature.AUTHENTICATION) # Добавили feature
+@allure.story(AllureStory.AUTHORIZATION) # Добавили story
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.AUTHENTICATION)
+@allure.sub_suite(AllureStory.AUTHORIZATION)
 class TestAuthorization:
     @pytest.mark.parametrize(
         "email, password",
@@ -16,12 +29,44 @@ class TestAuthorization:
             ("  ", "password")
         ]
     )
+    @allure.tag(AllureTag.USER_LOGIN) # Тег для конкретного теста
+    @allure.title("User login with wrong email or password")
+    @allure.severity(Severity.CRITICAL) # Добавили severity
     def test_wrong_email_or_password_authorization(self, login_page: LoginPage, email: str, password: str):
         login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
         login_page.login_form.fill(email=email, password=password)
         login_page.click_login_button()
         login_page.check_visible_wrong_email_or_password_alert()
 
+    @allure.tag(AllureTag.USER_LOGIN) # Тег для конкретного теста
+    @allure.title("User login with correct email and password") # Добавили заголовок
+    @allure.severity(Severity.BLOCKER) # Добавили severity
+    def test_successful_authorization(
+            self,
+            login_page: LoginPage,
+            dashboard_page: DashboardPage,
+            registration_page: RegistrationPage
+    ):
+        registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+        registration_page.registration_form.fill(email="user.name@gmail.com", username="username", password="password")
+        registration_page.click_registration_button()
+
+        dashboard_page.dashboard_toolbar.check_visible()
+        dashboard_page.navbar.check_visible("username")
+        dashboard_page.sidebar.check_visible()
+        dashboard_page.sidebar.click_logout()
+
+        login_page.login_form.fill(email="user.name@gmail.com", password="password")
+        login_page.click_login_button()
+
+        # Проверка элементов Dashboard после входа
+        dashboard_page.dashboard_toolbar.check_visible()
+        dashboard_page.navbar.check_visible("username")
+        dashboard_page.sidebar.check_visible()
+
+    @allure.tag(AllureTag.NAVIGATION) # Тег для конкретного теста
+    @allure.title("Navigation from login page to registration page") # Добавили заголовок
+    @allure.severity(Severity.NORMAL) # Добавили severity
     def test_navigate_from_authorization_to_registration(
             self,
             login_page: LoginPage,
--- tests/authentication/test_registration.py
+++ tests/authentication/test_registration.py
@@ -1,13 +1,25 @@
 import pytest
+import allure
+from allure_commons.types import Severity # Импортируем enum Severity из Allure
 
 from pages.authentication.registration_page import RegistrationPage
 from pages.dashboard.dashboard_page import DashboardPage
 from pages.authentication.login_page import LoginPage
+from tools.allure.epics import AllureEpic # Импортируем enum AllureEpic
+from tools.allure.features import AllureFeature # Импортируем enum AllureFeature
+from tools.allure.stories import AllureStory # Импортируем enum AllureStory
+from tools.allure.tags import AllureTag
 
 
 @pytest.mark.regression
 @pytest.mark.registration
+@allure.tag(AllureTag.REGRESSION, AllureTag.REGISTRATION)
+@allure.epic(AllureEpic.LMS) # Добавили epic
+@allure.feature(AllureFeature.AUTHENTICATION) # Добавили feature
+@allure.story(AllureStory.REGISTRATION) # Добавили story
 class TestRegistration:
+    @allure.title("Registration with correct email, username and password") # Добавили заголовок
+    @allure.severity(Severity.CRITICAL) # Добавили severity
     def test_successful_registration(self, dashboard_page: DashboardPage, registration_page: RegistrationPage):
         registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
         registration_page.registration_form.fill(
@@ -18,26 +30,3 @@ class TestRegistration:
         registration_page.click_registration_button()
 
         dashboard_page.dashboard_toolbar.check_visible()
-
-    def test_successful_authorization(
-            self,
-            login_page: LoginPage,
-            dashboard_page: DashboardPage,
-            registration_page: RegistrationPage
-    ):
-        registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-        registration_page.registration_form.fill(email="user.name@gmail.com", username="username", password="password")
-        registration_page.click_registration_button()
-
-        dashboard_page.dashboard_toolbar.check_visible()
-        dashboard_page.navbar.check_visible("username")
-        dashboard_page.sidebar.check_visible()
-        dashboard_page.sidebar.click_logout()
-
-        login_page.login_form.fill(email="user.name@gmail.com", password="password")
-        login_page.click_login_button()
-
-        # Проверка элементов Dashboard после входа
-        dashboard_page.dashboard_toolbar.check_visible()
-        dashboard_page.navbar.check_visible("username")
-        dashboard_page.sidebar.check_visible()
